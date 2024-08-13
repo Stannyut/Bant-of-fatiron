64b825167d1522d692d2ddb3510763eb7dc1e478
Stannyut <stanley.njuguna@student.moringaschool.com>
2024-08-13 10:03:26 +0300

    Code challenge

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -3,6 +3,7 @@ import React from "react";
 function Transaction({ transaction }) {
   const { date, description, category, amount } = transaction;
   return (
+    //This updates the table dynamically
     <tr>
       <td>{date}</td>
       <td>{description}</td>
